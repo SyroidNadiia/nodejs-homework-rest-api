b15d8d18992b1ffd2b2895a816116b44cdcfedea
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2023-07-10 11:28:56 +0300

    added Jimp

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const logger = require("morgan");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
+
 const contactsRouter = require("./routes/api/contacts");
 const authRouter = require("./routes/api/auth");
 const usersRouter = require("./routes/api/users");
@@ -15,6 +16,7 @@ dotenv.config();
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,10 +4,17 @@ const router = express.Router();
 
 const { ctrlWrapper } = require("../../helpers");
 const ctrl = require("../../controllers/users");
-const { authenticate, validation } = require("../../middlewares");
+const { authenticate, validation, upload} = require("../../middlewares");
 const { schemas } = require("../../models/user");
 
 router.get("/current", authenticate, ctrlWrapper(ctrl.getCurrent));
+
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrlWrapper(ctrl.updateAvatar)
+);
 
 router.patch(
   "/:id",
